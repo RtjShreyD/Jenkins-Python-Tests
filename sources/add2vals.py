7ce7c7146706793480ca5c29d7b8572201e276ab
rtjshreyd <shreyanshece1041@gmail.com>
2021-11-23 11:04:32 +0530

    A slightly modified state

--- sources/add2vals.py
+++ sources/add2vals.py
@@ -11,7 +11,7 @@ argnumbers = len(sys.argv) - 1
 if argnumbers == 2 :
     print("")
     print("The result is " + str(calc.add2(str(sys.argv[1]), str(sys.argv[2]))))
-    print("")
+    print("OK")
     sys.exit(0)
 
 if argnumbers != 2 :
